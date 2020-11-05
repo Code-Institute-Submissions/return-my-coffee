6ec14d08bfacc854c76462eff0154127ebd06c42
EllenSt264 <stafford.ellen@protonmail.com>
2020-11-05 17:28:18 +0000

    Fix multiple click card bug
    If the user clicks a card more than once it will register a match and break the game.
    
    Add code to check if the card has been clicked already and prevent a match caused by double clicking the card.
    
    Add resetBoard function for when users double click a card so cards can be clicked again on the next attempt to find a match.

--- assets/js/memory-game.js
+++ assets/js/memory-game.js
@@ -11,6 +11,7 @@ cards.forEach((card)=> {
 
 function checkCards() {
     if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add("flip");
     if (!hasFlippedCard) {
         hasFlippedCard = true;
@@ -25,6 +26,7 @@ function checkCards() {
             // It's a match!
             firstCard.removeEventListener("click", checkCards, false);
             secondCard.removeEventListener("click", checkCards, false);
+            resetBoard();
         }
         else {
             lockBoard = true;
@@ -33,7 +35,15 @@ function checkCards() {
                 firstCard.classList.remove("flip");
                 secondCard.classList.remove("flip");
                 lockBoard = false;
+                resetBoard();
             }, 800);
         }
     }
+}
+
+/*  When the user double clicks a card and then gets a failed match, 
+    the gameboard needs to reset to allow that same card to be clicked again */
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
