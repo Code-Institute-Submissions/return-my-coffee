5db91e4872c259bfeb34c6289bd67e35d6da3103
EllenSt264 <stafford.ellen@protonmail.com>
2020-11-05 16:01:24 +0000

    Add checkCards function to check for card matches
    Assign a firstCard and secondCard variable to each card that is clicked to implement game functionality.

--- assets/js/memory-game.js
+++ assets/js/memory-game.js
@@ -1,7 +1,35 @@
 const cards = document.querySelectorAll(".memory-card");
 
+// Define variables for checking cards
+var hasFlippedCard = false;
+var firstCard, secondCard;
+
 cards.forEach((card)=> {
-    card.addEventListener("click", function() {
-        card.classList.toggle("flip");
-    })
+    card.addEventListener("click", checkCards);
 })
+
+function checkCards() {
+    this.classList.add("flip");
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    }
+    else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // Do the cards match?
+        if (firstCard.dataset.cardname === secondCard.dataset.cardname) {
+            // It's a match!
+            firstCard.removeEventListener("click", checkCards, false);
+            secondCard.removeEventListener("click", checkCards, false);
+        }
+        else {
+            // It's not a match so flip the cards back
+            setTimeout(function() {
+                firstCard.classList.remove("flip");
+                secondCard.classList.remove("flip");
+            }, 800);
+        }
+    }
+}
