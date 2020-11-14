b6484798ce8b62003b4a97c6c3e942279c37177d
EllenSt264 <stafford.ellen@protonmail.com>
2020-11-14 19:00:04 +0000

    Refractor code
    Add menu.js to handle the start game menu for each game in order to prevent code from being repeated unnecessarily.
    
    Move the code that handles the start game menu from memory card's setup.js to menu.js.

--- assets/js/memory-game/setup.js
+++ assets/js/memory-game/setup.js
@@ -1,10 +1,3 @@
-// Game-start modal variables
-const openGame = document.querySelector("#startGameBtn");
-const easyBtn = document.querySelector("#easyBtn");
-const mediumBtn = document.querySelector("#mediumBtn");
-const hardBtn = document.querySelector("#hardBtn");
-const confirmSetting = document.querySelector(".setting-confirmation span");
-
 // End game - win/lose modal variables
 const winGameModal = document.querySelector("#winModal");
 const loseGameModal = document.querySelector("#loseModal");
@@ -31,8 +24,6 @@ The following source was used to fix this issue:
 
 //  Open Game
 
-var settingBtnClicked = false;
-
 function findYourCoffee() {
     if (settingBtnClicked === false) {
         alert("Please pick a setting before you play")
@@ -40,47 +31,4 @@ function findYourCoffee() {
     else if (settingBtnClicked === true) {
         window.location.replace("memory-game.html");
     }
-}
-
-// Game Difficulty Settings
-
-function checkForDifficultySetting() {
-    if (openGame) {
-        openGame.addEventListener("click", findYourCoffee);
-    }
-}
-
-checkForDifficultySetting();
-
-// Easy
-if (easyBtn) {
-    easyBtn.addEventListener("click", function(){
-        localStorage.setItem("countdownSpeed", 60);
-        localStorage.setItem("animateSpeed", 61000);
-        confirmSetting.style.display = "block";
-        confirmSetting.textContent = "easy";
-        settingBtnClicked = true;
-    });
-}
-
-// Medium
-if (mediumBtn) {
-    mediumBtn.addEventListener("click", function(){
-        localStorage.setItem("countdownSpeed", 40);
-        localStorage.setItem("animateSpeed", 41000);
-        confirmSetting.style.display = "block";
-        confirmSetting.textContent = "medium";
-        settingBtnClicked = true;
-    });
-}
-
-// Hard
-if (hardBtn) {
-    hardBtn.addEventListener("click", function(){
-        localStorage.setItem("countdownSpeed", 25);
-        localStorage.setItem("animateSpeed", 26000);
-        confirmSetting.style.display = "block";
-        confirmSetting.textContent = "hard";
-        settingBtnClicked = true;
-    });
 }
---
+++ assets/js/menu.js
@@ -0,0 +1,53 @@
+// Game-start modal variables
+const openGame = document.querySelector("#startGameBtn");
+const easyBtn = document.querySelector("#easyBtn");
+const mediumBtn = document.querySelector("#mediumBtn");
+const hardBtn = document.querySelector("#hardBtn");
+const confirmSetting = document.querySelector(".setting-confirmation span");
+
+// Game Difficulty Settings
+
+var settingBtnClicked = false;
+
+function checkForDifficultySetting() {
+    if (openGame) {
+        openGame.addEventListener("click", function() {
+            findYourCoffee();
+        });
+    }
+}
+
+checkForDifficultySetting();
+
+// Easy
+if (easyBtn) {
+    easyBtn.addEventListener("click", function(){
+        localStorage.setItem("countdownSpeed", 60);
+        localStorage.setItem("animateSpeed", 61000);
+        confirmSetting.style.display = "block";
+        confirmSetting.textContent = "easy";
+        settingBtnClicked = true;
+    });
+}
+
+// Medium
+if (mediumBtn) {
+    mediumBtn.addEventListener("click", function(){
+        localStorage.setItem("countdownSpeed", 40);
+        localStorage.setItem("animateSpeed", 41000);
+        confirmSetting.style.display = "block";
+        confirmSetting.textContent = "medium";
+        settingBtnClicked = true;
+    });
+}
+
+// Hard
+if (hardBtn) {
+    hardBtn.addEventListener("click", function(){
+        localStorage.setItem("countdownSpeed", 25);
+        localStorage.setItem("animateSpeed", 26000);
+        confirmSetting.style.display = "block";
+        confirmSetting.textContent = "hard";
+        settingBtnClicked = true;
+    });
+}
