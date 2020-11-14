ecd27ef60d9bf8a684a53884245714ca8e404bd8
EllenSt264 <stafford.ellen@protonmail.com>
2020-11-14 19:10:13 +0000

    Add game variables to setup.js and game.js

---
+++ assets/js/whack-a-mole/game.js
@@ -0,0 +1,10 @@
+/* 
+The following code for this game was based upon a video guide from the following source:
+"https://www.youtube.com/watch?v=RTb8icFiSfk&list=PLYElE_rzEw_sowQGjRdvwh9eAEt62d_Eu&index=2"
+*/
+
+let lastHole;
+let timeUp = false;
+let timeLimit = 2000;
+let score = 0;
+let countdown;
--- assets/js/whack-a-mole/setup.js
+++ assets/js/whack-a-mole/setup.js
@@ -1,5 +1,13 @@
-// Open Game from game menu
+// Constant game variables for whack-a-mole
+const holes = document.querySelectorAll(".hole");
+const moles = document.querySelector(".mole");
+const scoreBoard = document.querySelector(".score");
+const countdownBoard = document.querySelector(".time-left");
+const timeLeft = document.querySelector(".time-left span");
+const progressBar = document.querySelector(".inner-bar");
+const startGameBtn = document.querySelector("#startButton");
 
+// Open Game from game menu
 function findYourCoffee() {
     if (settingBtnClicked === false) {
         alert("Please pick a setting before you play")
