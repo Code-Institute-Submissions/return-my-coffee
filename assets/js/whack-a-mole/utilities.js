4e351adec2c4a7e4efe5c5a11fbacc06db011cfd
EllenSt264 <stafford.ellen@protonmail.com>
2020-11-17 13:28:33 +0000

    Refractor code
    Add utilities.js in whack-a-mole JS folder to handle the start game function and game scores.
    
    Move appropriate code from game.js to utlities.js.
    
    This will declutter and better organise the game's code and make it easier to understand.

---
+++ assets/js/whack-a-mole/utilities.js
@@ -0,0 +1,72 @@
+// Start game 
+
+startGameBtn.addEventListener("click", startGame);
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    score = 0;
+    timeUp = false;
+    molePopUp();
+
+    // Stop mole pop up when timer is finished
+    setTimeout(() => {
+        timeUp = true;
+    }, countdown * 1000)
+
+    let startCountdown = setInterval(() => {
+        countdown -= 1;
+        timeLeft.textContent = countdown;
+        if (countdown < 0) {
+            countdown = 0;
+            clearInterval(startCountdown);
+            checkHighScore();
+            countdownBoard.textContent = "Time's Up!"
+        }
+    }, 1000);
+    
+    progressBar.animate({
+        width: "0%"
+    }, animateSpeed)
+}
+
+// Count player score
+
+moles.forEach(mole => mole.addEventListener("click", whackMole));
+
+function whackMole(e) {
+    score ++;
+
+    // change mole image when hit
+    this.style.backgroundImage = "url('assets/img/whack-a-mole/angrymole.png')";
+    // disable clicks once a mole has been hit
+    this.style.pointerEvents = "none"; 
+
+    // Set value of timeoutSpeed
+    if (countdown == 60) {
+        timeoutSpeed = 800;
+    }
+    if (countdown == 40) {
+        timeoutSpeed = 800;
+    }
+    else if (countdown == 25) {
+        timeoutSpeed = 600;
+    }
+
+    // Set image back to default after being hit
+    setTimeout(() => {
+        this.style.backgroundImage = "url('assets/img/whack-a-mole/mole.png')";
+        this.style.pointerEvents = "all"; 
+    }, timeoutSpeed)
+
+    scoreBoard.textContent = score;
+}
+
+// Check high score
+
+function checkHighScore() {
+    if (score > localStorage.getItem("game1HighScore")) {
+        localStorage.setItem("game1HighScore", score);
+        highscore = score;
+        highScoreBoard.textContent = highscore;
+    }
+}
