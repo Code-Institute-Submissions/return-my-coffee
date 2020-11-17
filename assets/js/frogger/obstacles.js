bc9022124962c03309dce10876d58cd7e48f12d6
EllenSt264 <stafford.ellen@protonmail.com>
2020-11-17 19:52:06 +0000

    Add second car obstacle lane to obstacles.js
    Update obstacle JS class so that the new lane can move in the opposite direction.

--- assets/js/frogger/obstacles.js
+++ assets/js/frogger/obstacles.js
@@ -13,9 +13,18 @@ class Obstacle {
     }  
     update() {
         this.x += this.speed * gamespeed;
-        // Reset cars behind the left edge of the canvas
+        // Moving to the right
         if (this.x > canvas.width + this.width) {
-            this.x = 0 - this.width;
+            // Reset cars behind the left edge of the canvas
+            if (this.x > canvas.width + this.width) {
+                this.x = 0 - this.width;
+            }
+        }
+        // Speed is less than zero - moving to the left
+        else {
+            if (this.x < 0 - this.width) {     
+                this.x = canvas.width + this.width;     // '+ this.width' will give the cars a small delay before they appear again
+            }
         }
     }
 }
@@ -23,11 +32,18 @@ class Obstacle {
 // Add new obstacle to car array
 
 function initObstacles() {
+    // Lane 1
     for (let i = 0; i < 2; i++) {
         let x = i * 350;    
         carsArray.push(new Obstacle(x, canvas.height - grid * 2 - 20, grid * 2, grid, 1, "car"));
     }
+    // Lane 2
+    for (let i = 0; i < 2; i++) {
+        let x = i * 300;
+        carsArray.push(new Obstacle(x, canvas.height - grid * 3 - 20, grid * 2, grid, -2, "car"));
+    }
 }
+
 initObstacles();
 
 function handleObstacles() {
