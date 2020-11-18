4fdedf3b35cf94139c30027837df4f859054b56c
EllenSt264 <stafford.ellen@protonmail.com>
2020-11-18 10:33:36 +0000

    Update dust particle effect
    Add opacity to Dust class to make the effect gradually become invisible.
    
    Make the dust effect look like real dust rather than opaque grey circles, making it more natural and less obtrusive for the player.

--- assets/js/frogger/dust.js
+++ assets/js/frogger/dust.js
@@ -1,7 +1,7 @@
 class Dust {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+        this.x = x + 25;
+        this.y = y + 25;
         this.radius = Math.random() * 20 + 1;
         this.opacity = 1;
         // Positive numbers will make the ust go up
@@ -10,7 +10,7 @@ class Dust {
         this.directionY = Math.random() * 1 - 0.5;
     }
     draw() {
-        ctx3.fillStyle = "rgba(150, 150, 150, 1)";
+        ctx3.fillStyle = "rgba(150, 150, 150,"+ this.opacity + ")";
         ctx3.beginPath();
         ctx3.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx3.fill();
@@ -19,6 +19,12 @@ class Dust {
     update() {
         this.x += this.directionX;
         this.y += this.directionY;
+        if (this.opacity > 0.1) {
+            this.opacity -= 0.9;
+        }
+        if (this.radius > 0.15) {
+            this.radius -= 0.14;
+        }
     }
 }
 
