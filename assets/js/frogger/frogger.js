6d0abe25edf3bec5d3dd40ce5200f061e3c61e73
EllenSt264 <stafford.ellen@protonmail.com>
2020-11-18 12:36:30 +0000

    Fix start game car collision
    Fix car collision caused by the frog's y-axis starting position which was too high.

--- assets/js/frogger/frogger.js
+++ assets/js/frogger/frogger.js
@@ -5,7 +5,7 @@ class Frogger {
         this.width = this.spriteWidth / 5;
         this.height = this.spriteHeight / 5;
         this.x = canvas.width / 2 - this.width / 2;
-        this.y = canvas.height - this.height - 50;
+        this.y = canvas.height - this.height - 40;
         this.moving = false;
         this.frameX = 0;
         this.frameY = 0;
