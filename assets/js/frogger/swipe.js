be312bd9e39a80fe1b7e231d41f68d6d91b10ed0
EllenSt264 <stafford.ellen@protonmail.com>
2020-11-24 18:50:52 +0000

    Add swipe.js to handle touchscreen controls in frogger

---
+++ assets/js/frogger/swipe.js
@@ -0,0 +1,70 @@
+const gameboard = document.querySelector(".frogger-board");
+
+/*
+** The following code was based upon this source:
+    "https://stackoverflow.com/questions/53192433/how-to-detect-swipe-in-javascript"
+
+    Which was taken from here:
+    "https://www.kirupa.com/html5/detecting_touch_swipe_gestures.htm"
+
+    ** Original Author: Kirupa Chinnathambi
+*/
+
+gameboard.addEventListener("touchstart", startTouch, false);
+gameboard.addEventListener("touchmove", moveTouch, false);
+
+// Swipe Up / Down / Left / Right
+var initialX = null;
+var initialY = null;
+
+function startTouch(e) {
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+}
+
+function moveTouch(e) {
+    if (initialX === null) {
+        return;
+    }
+
+    if (initialY === null) {
+        return;
+    }
+
+    var currentX = e.touches[0].clientX;
+    var currentY = e.touches[0].clientY;
+
+    var diffX = initialX - currentX;
+    var diffY = initialY - currentY;
+
+    frogger.moving = false;
+        frogger.frameX = 0;
+    
+    // Swipe horizontally
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+        // Swipe left
+        if (diffX > 0) {
+        console.log("swiped left");
+        } 
+        // Swipe right
+        else {
+        console.log("swiped right");
+        }  
+    } 
+    // Swipe vertically
+    else {
+        // Swipe up
+        if (diffY > 0) {
+        console.log("swiped up");
+        } 
+        // Swipe down
+        else {
+        console.log("swiped down");
+        }  
+    }
+
+    initialX = null;
+    initialY = null;
+
+    e.preventDefault();
+}
