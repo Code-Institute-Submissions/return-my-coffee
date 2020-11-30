8314e9c7747aa7b39c1efc6490ecf3a5d0764e4f
EllenSt264 <stafford.ellen@protonmail.com>
2020-11-30 11:46:05 +0000

    Add missing semicolon to setup.js for whack-a-mole

--- assets/js/whack-a-mole/setup.js
+++ assets/js/whack-a-mole/setup.js
@@ -2,7 +2,7 @@
 const holes = document.querySelectorAll(".hole");
 const moles = document.querySelectorAll(".mole");
 const scoreBoard = document.querySelector(".score span");
-const highScoreBoard = document.querySelector(".highscore span")
+const highScoreBoard = document.querySelector(".highscore span");
 const countdownBoard = document.querySelector(".time-left");
 const timeLeft = document.querySelector(".time-left span");
 const progressBar = document.querySelector(".inner-bar");
