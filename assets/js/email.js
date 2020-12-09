fc287e2685245ac6406e4be1c1c7e6da97f353f7
EllenSt264 <stafford.ellen@protonmail.com>
2020-12-09 20:27:13 +0000

    Add alert box to provide feedback once sending an email
    Add Bootbox JS framework to index.html to style the alert box generated in email.js.
    
    Update Bootbox alert box styling with custom CSS in style.css.

--- assets/js/email.js
+++ assets/js/email.js
@@ -7,8 +7,21 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     event.preventDefault();
     emailjs.sendForm("ellenst264", "return_mycoffee", this)
         .then(function() {
-            console.log("SUCCESS!");
+            console.log("SUCCESS! Email sent");
+            bootbox.alert({
+                message: "<p class='text-center confirm-send'>Thank you. Your message has been sent. I will get back to as soon as I can!</p>",
+                onEscape: true,
+                backdrop: true,
+                buttons: {
+                    ok: {
+                        label: "okay",
+                        className: "btn-confirm-send"
+                    }
+                }
+            })
+
         }, function(error) {
             console.log("FAILED...", error);
-        });
+            alert("Oops, something went wrong..." + JSON.stringify(error));
+        })
 });
